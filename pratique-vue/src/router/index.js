bae1d5c757bd18f95998d64787523f459dbfcd13
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2021-08-13 08:49:41 +0200

    dernier test d'intégration des router

--- pratique-vue/src/router/index.js
+++ pratique-vue/src/router/index.js
@@ -1,12 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HelloWorld from '../components/HelloWorld.vue' // ".." rajouté ici
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Contact from '../views/Contact.vue'
 
-export default createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      component: HelloWorld,
-    }
-  ]
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
 })
+
+export default router
